a2ec5cf4aea6aa6803cf7bf56973608bcaf7adf8
francimaraoliveira <61793314+francimaraoliveira@users.noreply.github.com>
2023-08-13 23:37:11 -0300

    Add files via upload

---
+++ nest js-mvc/Projeto/src/app.controller.ts
@@ -0,0 +1,37 @@
+import {
+  Controller,
+  Get,
+  Query,
+  Render,
+} from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('/')
+  @Render('index')
+  hello(@Query('nome') nome = 'Visitante') {
+    const context = {
+      nome,
+      qtd_letras: nome.length,
+    };
+    return context;
+  }
+
+  @Get('form-imc')
+  @Render('imc')
+  formIMC(@Query('imc') imc: string) {
+    const registros = this.appService.obterRegistrosIMC();
+    const context = { imc, registros };
+
+    return context;
+  }
+
+  /* @Post('calcular-imc')
+  calcularIMC(@Res() res: Response, @Body() input: CalcularIMCInput) {
+    const imc = this.appService.calcularIMC(input);
+    res.redirect(`/form-imc?imc=${imc}`);
+  } */
+}
